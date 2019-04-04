a5b16661ec10a64c9b06c3944f29721ce1f94fe4
Bamoooh <45263225+Bamoooh@users.noreply.github.com>
2019-04-04 14:05:31 +0300

    commented checker

--- rento/lib/api/DatesChecker.dart
+++ rento/lib/api/DatesChecker.dart
@@ -32,10 +32,47 @@ class DatesChecker
 
 
   static void destroy()
+import 'dart:async';
+import 'FirestoreServices.dart';
+class DatesChecker
+{
+  static Timer availabilityTimer;
+  static Timer requestsTimer;
+  String userID;
+  DatesChecker(this.userID){
+    availabilityTimer = new Timer.periodic(Duration(seconds: 1), (timer)=>availabilityChecks(timer));
+    requestsTimer = new Timer.periodic(Duration(seconds: 1), (timer)=>requestsCheck(timer));
+    print("INITED CHECKER");
+  }
+
+  void availabilityChecks(Timer t)
   {
-    availabilityTimer.cancel();
-    requestsTimer.cancel();
+    // FirestoreServices.getItemList().forEach((snapshot){
+    //   snapshot.documents.forEach((doc){
+        
+    //   });
+    // });
+  }
+
+  void requestsCheck(Timer t)
+  {
+    // FirestoreServices.getRequests().forEach((snapshot){
+    //   snapshot.documents.forEach((doc){
+    //     String state = doc.data['state'];
+    //   });
+    // });
+
+  }
+
+
+  static void destroy()
+  {
+    availabilityTimer == null ? null : availabilityTimer.cancel();
+    requestsTimer == null ? null : requestsTimer.cancel();
+    
     print("DESTROYED CHECKER");
   }
+
+}
 
 }
